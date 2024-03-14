1e9bfe5c9e79c00e5d73ca6afd15b1d209461e23
eccv2024mc2 <mcceccv2024@gmail.com>
2024-03-14 07:18:14 +0000

    placeholder

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var VIDEO_ASPECT_RATIO = 16.0 / 9.0;
 
 
 
+$("#more-video").on('loadedmetadata', function () {
+    this.width = this.videoWidth;
+    this.height = this.videoHeight;
+    console.log(this.width, this.height);
+  });
